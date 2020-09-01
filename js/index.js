f8d2a1e89fc6d7e7075d27daa8410c334d2ed016
Rahul Gupta <52673814+RAHUL1115@users.noreply.github.com>
2020-09-01 13:29:23 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+window.onload = (e)=>{
+    document.getElementById("loader-wrapper").style.animation = "loader-wrapper 1s linear forwards";
+};
+
+window.onscroll = (e)=>{
+    if (document.body.scrollTop > h/10 || document.documentElement.scrollTop > h/10) {
+        //after scroll
+        document.getElementById("navbar").classList.add('shadow-lg');
+    } else {
+        //before scroll
+        document.getElementById("navbar").classList.remove('shadow-lg');
+    }
+};
+
+window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+});
+
+function scrollToId(id){
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
