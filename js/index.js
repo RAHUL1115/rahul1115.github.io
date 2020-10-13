38cd1cfd3626470acb6090a61d13da27fec203d6
Rahul Gupta <52673814+RAHUL1115@users.noreply.github.com>
2020-10-13 14:36:50 +0530

    ghjhasgd

--- js/index.js
+++ js/index.js
@@ -2,27 +2,47 @@ var w = window.innerWidth || document.documentElement.clientWidth || document.bo
 var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 var touchstart = [0, 0];
 var touchend = [0, 0];
-var xpos = 0;
+xpos = 0;
 
-var gesuredZone = document.getElementById('main');
+// let topwidth = (100 - ((document.getElementById('socialicon').clientHeight - document.getElementById('topnav').clientHeight) / (h / 100))) / 2;
+let topwidth = (100 - ((document.getElementById('socialicon').clientHeight - 0) / (h / 100))) / 2;
+document.getElementById('socialicon').style.top = topwidth + '%';
+let width = (100 - (document.getElementById('downnav').clientWidth / (h / 100))) / 2;
+document.getElementById('downnav').style.left = width + '%';
 
-gesuredZone.addEventListener('touchstart', function (event) {
+var swipeZone = document.getElementById('main');
+
+//for mobile
+swipeZone.addEventListener('touchstart', function (event) {
     touchstart[0] = event.changedTouches[0].screenX;
     touchstart[1] = event.changedTouches[0].screenY;
 }, false);
 
-gesuredZone.addEventListener('touchend', function (event) {
+swipeZone.addEventListener('touchend', function (event) {
     touchend[0] = event.changedTouches[0].screenX;
     touchend[1] = event.changedTouches[0].screenY;
-    handleGesure();
+    handleSwipe();
 }, false);
 
-function handleGesure() {
+//for desktop
+swipeZone.addEventListener('mousedown', function (event) {
+    // console.log('mouse down');
+    touchstart[0] = event.screenX;
+    touchstart[1] = event.screenY;
+}, false);
+
+swipeZone.addEventListener('mouseup', function (event) {
+    // console.log('mouse up');
+    touchend[0] = event.screenX;
+    touchend[1] = event.screenY;
+    handleSwipe();
+}, false);
+
+function handleSwipe() {
     let xdif = touchstart[0] - touchend[0];
     let ydif = touchstart[1] - touchend[1];
     if (xdif ** 2 > ydif ** 2) {
-
-        if (xdif < -20) {
+        if (xdif < -24) {        
             let el = document.getElementsByClassName('el');
             if (xpos == 0) {
                 xpos = 0;
@@ -32,8 +52,9 @@ function handleGesure() {
             for (let i = 0; i < el.length; i++) {
                 el[i].style.transform = 'translateX(' + xpos + '%)';
             }
+            dotchange();
         }
-        if (xdif > 20) {
+        if (xdif > 24) {
             let el = document.getElementsByClassName('el');
             if (xpos == -200) {
                 xpos = -200;
@@ -43,9 +64,47 @@ function handleGesure() {
             for (let i = 0; i < el.length; i++) {
                 el[i].style.transform = 'translateX(' + xpos + '%)';
             }
+            dotchange();
         }
     } 
     // else {
-    //     alert('swiped in y dir');
+    //     if (ydif < -20) {
+    //         console.log('down');
+    //     }
+    //     if (ydif > 20) {
+    //         console.log('up');
+    //     }
     // }
+}
+
+function dotchange(){
+    let dot = document.getElementsByClassName('dot');
+    switch (xpos) {
+        case 0:
+            dot[0].classList.remove('text-3xl');
+            dot[0].classList.add('text-2xl');
+            dot[1].classList.add('text-3xl');
+            dot[2].classList.remove('text-2xl');
+            dot[2].classList.add('text-3xl');
+            dot[2].classList.remove('text-2xl');
+            break;
+        case -100:
+            dot[0].classList.add('text-3xl');
+            dot[0].classList.remove('text-2xl');
+            dot[1].classList.remove('text-3xl');
+            dot[1].classList.add('text-2xl');
+            dot[2].classList.add('text-3xl');
+            dot[2].classList.remove('text-2xl');
+            break;
+        case -200:
+            dot[0].classList.add('text-3xl');
+            dot[0].classList.remove('text-2xl');
+            dot[1].classList.add('text-3xl');
+            dot[1].classList.remove('text-2xl');
+            dot[2].classList.remove('text-3xl');
+            dot[2].classList.add('text-2xl');
+            break;
+        default:
+            break;
+    }
 }
