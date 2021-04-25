060e048c4c8c591358eb41c84be3a07ff443f133
Rahul Gupta <52673814+RAHUL1115@users.noreply.github.com>
2021-04-25 21:40:21 +0530

    efsfe

--- js/index.js
+++
@@ -1,6 +0,0 @@
-var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-// let topwidth = (100 - ((document.getElementById('socialicon').clientHeight - document.getElementById('topnav').clientHeight) / (h / 100))) / 2;
-let topwidth = (100 - ((document.getElementById('socialicon').clientHeight - 0) / (h / 100))) / 2;
-document.getElementById('socialicon').style.top = topwidth + '%';
