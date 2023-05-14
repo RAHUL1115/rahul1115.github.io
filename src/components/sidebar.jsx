3a7f7a702e9b36c7921ab9694971a787ed660147
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2023-05-14 21:35:06 +0530

    tailwind css fixed

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Sidebar(){
   return (
-    <div class="flex flex-col h-full justify-center items-center space-y-8">
+    <div className="flex flex-col h-full justify-center items-center space-y-8">
       <Image
         width={112}
         height={112}
@@ -11,25 +11,25 @@ export default function Sidebar(){
         src="/img/rahul.jpg"
         alt="Rahul Gupta Image"
       ></Image>
-      <div class="flex flex-col items-center space-y-3">
+      <div className="flex flex-col items-center space-y-3">
         <Link
-          class="font-semibold hover:underline text-black transition-all"
+          className="font-semibold hover:underline text-black transition-all"
           href="/"
         >About</Link>
         <Link
-          class="font-semibold hover:underline text-black transition-all"
+          className="font-semibold hover:underline text-black transition-all"
           href="/projects"
         >
           Projects
         </Link>
         <Link
-          class="font-semibold hover:underline text-black transition-all"
+          className="font-semibold hover:underline text-black transition-all"
           href="/articles"
         >
           Articles
         </Link>
         <Link
-          class="font-semibold hover:underline text-black transition-all"
+          className="font-semibold hover:underline text-black transition-all"
           href="/contact"
         >
           Contact
--- src/layouts/base.jsx
+++ src/layouts/base.jsx
@@ -5,11 +5,11 @@ Sidebar
 export default function Base({ children }) {
   return (
     <main>
-      <div class="flex min-h-screen md:relative">
-        <div class="md:w-[25%] md:bg-orange-300 md:p-4 md:max-h-screen md:sticky md:top-0 hidden md:block">
+      <div className="flex min-h-screen md:relative">
+        <div className="md:w-[25%] md:bg-orange-300 md:p-4 md:max-h-screen md:sticky md:top-0 hidden md:block">
           <Sidebar />
         </div>
-        <div class="md:w-[75%] p-4">{children}</div>
+        <div className="md:w-[75%] p-4">{children}</div>
       </div>
     </main>
   );
