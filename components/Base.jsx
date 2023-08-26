a485a3a237cac0ec7374a3ce80bf0bf226b994b2
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2023-08-26 13:23:36 +0530

    color change

--- components/Base.jsx
+++ components/Base.jsx
@@ -6,7 +6,7 @@ export default function Base({ children }) {
   return (
     <main>
       <div className="flex min-h-screen md:relative">
-        <div className="md:w-[25%] md:bg-orange-300 md:p-4 md:max-h-screen md:sticky md:top-0 hidden md:block">
+        <div className="md:w-[25%] md:bg-blue-300 md:p-4 md:max-h-screen md:sticky md:top-0 hidden md:block">
           <Sidebar />
         </div>
         <div className="md:w-[75%] p-4">{children}</div>
