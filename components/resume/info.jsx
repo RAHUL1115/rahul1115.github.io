ac3eac6feaf41efe1067e29a8ce56e56a16c12de
Rahul Gupta <rahulgupta.ra.@gmail.com>
2026-02-19 16:17:03 +0530

    feat: add Info component displaying resume printing instructions

--- components/resume/info.jsx
+++ components/resume/info.jsx
@@ -1,12 +1,19 @@
 export default function Info() {
   return (
-    <div className="print:hidden text-red-500 text-sm">
-      <p className="flex gap-2">
+    <div className="print:hidden text-sm flex items-center justify-between gap-4 mb-1">
+      <p className="flex gap-2 text-red-500">
         <span>steps to print this:</span>
         <span>(1) press ctrl+p</span>
         <span>(2) select color print & page size to A4</span>
         <span>(3) Set Margine to none & and scale to actual-size</span>
       </p>
+      {/* <a
+        href="/Rahul-Gupta.pdf"
+        download
+        className="text-red-500 underline shrink-0"
+      >
+        download stale pdf
+      </a> */}
     </div>
   );
 }
